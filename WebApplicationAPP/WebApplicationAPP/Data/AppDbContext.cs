a9dbbcadaf4bde94e8e21259e0207a4680277b9c
GilbertTencio <gtencio50750@gmail.com>
2026-02-27 19:34:54 -0600

    AperturaSolucion

---
+++ WebApplicationAPP/WebApplicationAPP/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using WebApplicationAPP.Models;
+
+namespace WebApplicationAPP.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+       // Clase;
+       // public DbSet<Persona> Persona { get; set; }
+       // public DbSet<Cliente> Cliente { get; set; }
+       // public DbSet<Inventario> Inventario { get; set; }
+
+    }
+}
