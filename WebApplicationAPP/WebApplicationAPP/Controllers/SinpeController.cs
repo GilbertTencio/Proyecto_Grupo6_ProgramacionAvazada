7d6d22e097c8e5fe57bf01348c7e4b004e71555b
saraas1411 <sara.as1411@gmail.com>
2026-03-19 17:06:18 -0600

    Modulo de sinpes

---
+++ WebApplicationAPP/WebApplicationAPP/Controllers/SinpeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplicationAPP.Bussines;
+using WebApplicationAPP.Models;
+
+namespace WebApplicationAPP.Controllers
+{
+    public class SinpeController : Controller
+    {
+        private readonly SinpeBusiness _business;
+
+        public SinpeController(SinpeBusiness business)
+        {
+            _business = business;
+        }
+        public IActionResult Index()
+        {
+            var sinpes = _business.GetAll();
+            return View(sinpes);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Sinpe sinpe)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(sinpe);
+            }
+
+            var creado = _business.Add(sinpe);
+
+            if (!creado)
+            {
+                ModelState.AddModelError("", "Verifique que el teléfono de destino este registrado, que la caja se encuentre activa y que el valor del monto sea mayor a 0.");
+                return View(sinpe);
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ WebApplicationAPP/WebApplicationAPP/Repositories/ISinpeRepository.cs
@@ -0,0 +1,13 @@
+﻿using WebApplicationAPP.Models;
+
+namespace WebApplicationAPP.Repositories
+{
+    public interface ISinpeRepository
+    {
+        List<Sinpe> GetAllSinpes();
+        Sinpe GetByTelefonoDestinatario(string telefonoDestino);
+        List<Sinpe> GetByComercio(int idCaja);
+        void AddSinpe(Sinpe sinpe);
+        Caja GetCaja(int idCaja);
+    }
+}
